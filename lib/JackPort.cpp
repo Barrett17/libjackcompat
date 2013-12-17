4e1f78f6be4e770890cabc39c4832c610aee1027
Barrett <b.vitruvio@gmail.com>
2013-12-17 03:19:24 +0100

    Updated copyright notice.

--- lib/JackPort.cpp
+++ lib/JackPort.cpp
@@ -1,7 +1,22 @@
 /*
- * Copyright 2013 Dario Casalinuovo. All rights reserved.
- * Distributed under the terms of the GPL License.
- */
+    Copyright 2013 Dario Casalinuovo. All rights reserved.
+
+    This file is part of libjackcompat.
+
+    libjackcompat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libjackcompat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libjackcompat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <string.h>
 
 #include "JackPort.h"
--- lib/WrapperDefs.h
+++ lib/WrapperDefs.h
@@ -1,16 +1,30 @@
 /*
- * Copyright 2013 Dario Casalinuovo. All rights reserved.
- * Distributed under the terms of the GPL License.
- */
-#ifndef _WRAPPERDEFS_H_
-#define _WRAPPERDEFS_H_
+    Copyright 2013 Dario Casalinuovo. All rights reserved.
+
+    This file is part of libjackcompat.
+
+    libjackcompat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libjackcompat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libjackcompat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBJACKCOMPAT_DEFS_H_
+#define _LIBJACKCOMPAT_DEFS_H_
 
 #define LIB_EXPORT __attribute__((visibility("default")))
 
 // Sizes constants
 #define WRAPPER_CLIENT_NAME_SIZE 64
 #define WRAPPER_PORT_NAME_SIZE 64
-
 
 // Preferred values (those should be set by a config gui or 
 // a settings file.
@@ -19,4 +33,4 @@
 
 #define WRAPPER_PREFERRED_FORMAT media_raw_audio_format::B_AUDIO_FLOAT
 
-#endif	// _WRAPPERDEFS_H_
+#endif
