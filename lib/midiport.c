f953f1db8ff121a27916347441a7c7e26f76be52
Dario Casalinuovo <b.vitruvio@gmail.com>
2015-07-09 00:25:09 +0200

    Add empty implementations for midi API.

--- lib/midiport.c
+++ lib/midiport.c
@@ -19,30 +19,54 @@
 
 #include "jack/midiport.h"
 
+
+uint32_t
+jack_midi_get_event_count(void* buffer)
+{
+	return 0;
+}
+
+
+int
+jack_midi_event_get(jack_midi_event_t* event, void* buffer,
+	uint32_t event_index)
+{
+	return -1;
+}
+
+
+size_t
+jack_midi_max_event_size(void* buffer)
+{
+	return -1;
+}
+
+
+jack_midi_data_t*
+jack_midi_event_reserve(void* buffer,
+	jack_nframes_t time, size_t data_size)
+{
+	return NULL;
+}
+
+
+int
+jack_midi_event_write(void* buffer,
+	jack_nframes_t time, const jack_midi_data_t* data,
+	size_t data_size)
+{
+	return -1;
+}
+
+
 void
-jack_midi_clear_buffer(void* port_buffer)
-{
-}
-
-
-int
-jack_midi_event_get(jack_midi_event_t* event,
-                    void*        port_buffer,
-                    uint32_t    event_index)
-{
-}
-
-
-int
-jack_midi_event_write(void* port_buffer,
-                      jack_nframes_t time,
-                      const jack_midi_data_t* data,
-                      size_t data_size)
+jack_midi_clear_buffer(void* buffer)
 {
 }
 
 
 uint32_t
-jack_midi_get_event_count(void* port_buffer)
+jack_midi_get_lost_event_count(void* buffer)
 {
+	return 0;
 }
