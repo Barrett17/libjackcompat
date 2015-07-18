5f553ca1ea570550d7b6bc0e1719d6f0a4f0dec8
Dario Casalinuovo <b.vitruvio@gmail.com>
2015-07-18 18:15:42 +0200

    Add intclient empty definitions

---
+++ lib/intclient.cpp
@@ -0,0 +1,49 @@
+/*
+    Copyright 2015 Dario Casalinuovo. All rights reserved.
+
+    This file is part of libjackcompat.
+
+    libjackcompat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libjackcompat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libjackcompat.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "jack/intclient.h"
+
+
+LIB_EXPORT jack_intclient_t
+jack_internal_client_load(jack_client_t* client,
+	const char* client_name, jack_options_t options,
+	jack_status_t* status, ...)
+{
+}
+
+
+LIB_EXPORT jack_status_t
+jack_internal_client_unload(jack_client_t* client,
+jack_intclient_t intclient)
+{
+}
+
+
+LIB_EXPORT char
+jack_get_internal_client_name(jack_client_t* client,
+	jack_intclient_t intclient)
+{
+}
+
+
+LIB_EXPORT jack_intclient_t
+jack_internal_client_handle(jack_client_t* client,
+	const char* client_name, jack_status_t* status)
+{
+}
